93c9adbf4f98b10d110ccb72ca71cd2b9db27fca
soumya-b019 <100502907+soumya-b019@users.noreply.github.com>
2023-02-26 23:59:03 +0530

    Create script.js

---
+++ Redirect to a particular section on same page using HTML/script.js
@@ -0,0 +1,11 @@
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", ()=>{
+    if(window.scrollY > 100){
+        toTop.classList.add("active");
+    }
+    else{
+        toTop.classList.remove("active");
+    }
+})
